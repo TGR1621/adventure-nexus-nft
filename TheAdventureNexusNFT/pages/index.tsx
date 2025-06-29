8658cec3ee27ae691f5c7cfba9a49fe810cf9532
TGR1621 <119565536+TGR1621@users.noreply.github.com>
2025-06-29 19:10:33 +0700

    Delete TheAdventureNexusNFT directory

--- TheAdventureNexusNFT/pages/index.tsx
+++
@@ -1,8 +0,0 @@
-export default function Home() {
-    return (
-      <main className="p-10 text-center">
-        <h1 className="text-4xl font-bold mb-4">Selamat Datang di The Adventure Nexus NFT 🌐</h1>
-        <p className="text-lg">Jelajahi, koleksi, dan jual NFT unik di Nexus Testnet!</p>
-      </main>
-    );
-  }
